102da446d2be388bafef683ae6d7744037f0b5db
Paaluu <paulo_alves_vaccari@yahoo.se>
2025-01-17 12:54:07 +0100

    change footer

--- src/components/pages/Footer.jsx
+++ src/components/pages/Footer.jsx
@@ -3,6 +3,11 @@ import '../styles/Footer.css'
 
 export default function Footer() {
   return (
-    <div className='footer'>Footer is here</div>
+    <div className='footer'>
+    <a href="https://www.instagram.com" target="_blank"> Instagram   </a><a>    </a>
+    <a href="https://www.facebook.com" target="_blank">   Facebook   </a>
+    <a href="mailto:GG-Odds@mail.se">   Contact via Email </a>
+    </div>
+
   )
 }
